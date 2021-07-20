9936a1493b8691ac540908710ef040687c00f4ad
morita-01 <morita_0963055@yahoo.co.jp>
2021-07-20 12:34:11 +0000

    2021072021

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,5 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+
+Refile.secret_key = '2d80e66a920b9d9c48697ec0084bd48847ee3ae61497f147f2382f94e3cd9c315f32d819527095d3441c2755498f8dbe236aec426667aa9f1e88b3bb76a68df7'

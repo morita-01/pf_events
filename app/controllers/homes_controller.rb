d7ec3696efe79445f82d2573e2ccd84ee7cce2b9
morita-01 <morita_0963055@yahoo.co.jp>
2021-08-08 11:52:54 +0000

    20210808

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,5 @@
 class HomesController < ApplicationController
+    #トップ画面処理
     def top
         @events = Event.all.order(id: :desc).limit(3)
     end
--- app/controllers/publics/favorites_controller.rb
+++ app/controllers/publics/favorites_controller.rb
@@ -1,4 +1,5 @@
 class Publics::FavoritesController < ApplicationController
+  #いいね追加処理
   def create
     @event = Event.find(params[:event_id])
     favorite = current_user.favorites.new(event_id: @event.id)
@@ -6,6 +7,7 @@ class Publics::FavoritesController < ApplicationController
     #redirect_to request.referer
   end
 
+  #いいね解除処理
   def destroy
     @event = Event.find(params[:event_id])
     favorite = current_user.favorites.find_by(event_id: @event.id)

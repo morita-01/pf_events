6be14d8ffd8930a5ac593b36c47506870bb2e54a
morita-01 <morita_0963055@yahoo.co.jp>
2021-08-18 09:36:59 +0000

    20210818

--- app/controllers/searches_controller.rb
+++
@@ -1,9 +0,0 @@
-class SearchesController < ApplicationController
-  def search
-    search = params[:search]
-    word = params[:word]
-    @events = Event.looks(search, word)
-  #@keyword = params[:keyword]
-  #render "search"
-  end
-end
